7b57b29ad73ccf1b39f0acf5a9899b011a76b468
prbh695a <prateek.bhat.it@gmail.com>
2022-01-06 18:39:20 +0100

    Second commit

---
+++ cookies.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, make_response, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return render_template("setcookies.html")
+
+
+@app.route('/setcookie', methods=["POST", "GET"])
+def setcookie():
+    if request.method == "POST":
+        user = request.form['nm']
+        resp = make_response(render_template("readcookie.html"))
+        resp.set_cookie('userID', user)
+        return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return "<h1> Welcome " + name + "</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
